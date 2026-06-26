b62b75d02c2467d8d0520a65ebd70cefa37cbf17
RAGHAV <raghavpratap987654@gmail.com>
2026-06-26 17:32:19 +0530

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import './utils/colorPatch';
+import './utils/cssColorPatch';
 
 if (typeof (Object as any).hasOwn !== 'function') {
   (Object as any).hasOwn = function (object: any, property: PropertyKey): boolean {
